0faa7b4fde357260b6dfa62df6fd0b019c4949c2
François Fornaciari <francois.fornaciari@gmail.com>
2024-12-07 21:38:43 +0100

    update

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -5,7 +5,7 @@ import { loadFragment } from '../fragment/fragment.js';
 export default async function decorate(block) {
   const content = document.createRange().createContextualFragment(`
     <nav class="nav-menu">
-        <a href="#" class="nav-logo"><img src="../../icons/logo.svg"></img></a>
+        <a href="#" class="nav-logo">BRAND</img></a>
         <button class="hamburger">
             <span></span>
             <span></span>
