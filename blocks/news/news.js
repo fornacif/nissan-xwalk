58b821a6837805d39c9ea2fedaf82458c9fce22c
François Fornaciari <francois.fornaciari@gmail.com>
2024-12-12 21:31:49 +0100

    update

--- blocks/news/news.js
+++ blocks/news/news.js
@@ -1,26 +1,28 @@
 import { readBlockConfig } from '../../scripts/aem.js';
-import { optimizeImageSrc } from '../../scripts/extension.js';
+import { getValueByKey, getFormattedDate, transformImageSrc, loadArticles } from '../../scripts/extension.js';
 
 export default async function decorate(block) {
     const config = readBlockConfig(block);
 
-    const articles = [];
-
-        /*var article = document.createElement('article');
-        article.className = 'article-card';
-        article.innerHTML = `
-            <img src="${imageSrc}" alt="Nissan Warranty" class="article-image">
+    const articles = await loadArticles();
+    const articleElements = [];
+    for (const article of articles) {
+        var articleElement = document.createElement('article');
+        articleElement.className = 'article-card';
+        const imageScr = await transformImageSrc(article.image['_path']);
+        const dateString = await getValueByKey(article['_metadata'].calendarMetadata, 'jcr:created', 'name');
+        const formattedDate = getFormattedDate(dateString);
+        articleElement.innerHTML = `
+            <img src="${imageScr}" alt="Alt" class="article-image">
             <div class="article-content">
-                <div class="article-date">December 9, 2024</div>
+                <div class="article-date">${formattedDate}</div>
                 <span class="article-category">Services</span>
-                <h2 class="article-title" data-aue-prop="title" data-aue-type="text">${title}</h2>
-                <p class="article-excerpt">Launched last year, the Nissan Privilege Warranty program has already rewarded 50,000 customers for their loyalty. Deployed across Nissan's dealer network, this offer has found widespread success.</p>
+                <h2 class="article-title" data-aue-prop="title" data-aue-type="text">${article.title}</h2>
+                <p class="article-excerpt">${article.description.html}</p>
                 <a href="#" class="article-link">Read more</a>
             </div>`;
-        moveInstrumentation(row, article);
-
-        articles.push(article)*/
-
+            articleElements.push(articleElement)
+    }
 
     const content = document.createRange().createContextualFragment(`
         <section class="hero-news">
@@ -43,14 +45,14 @@ export default async function decorate(block) {
 
     block.textContent = '';
 
-    const imageScr = optimizeImageSrc(config.image);
+    const imageScr = await transformImageSrc(config.image);
     const heroNews = content.querySelector('.hero-news');
     heroNews.style.backgroundImage = `url(${imageScr})`;
     heroNews.style.backgroundPosition = 'center';
     heroNews.style.backgroundSize = 'cover';
 
     const articleList = content.querySelector('.article-list');
-    articles.forEach(article => articleList.append(article));
+    articleElements.forEach(articleElement => articleList.append(articleElement));
 
     block.append(content);
 }
--- scripts/extension.js
+++ scripts/extension.js
@@ -1,3 +1,49 @@
-export function optimizeImageSrc(imageSrc) {
+async function getConfigs() {
+    const configs = window.localStorage.getItem('configs');
+    if (configs) {
+        return JSON.parse(configs);
+    } else {
+        const response = await fetch('/configs.json');
+        if (!response.ok) {
+            throw new Error('Failed to fetch configs');
+        }
+        const configs = await response.json();
+        window.localStorage.setItem('configs', JSON.stringify(configs));
+        return configs;
+    }
+}
+
+async function getConfigValueByKey(key) {
+    const configs = await getConfigs();
+    return getValueByKey(configs.data, key);
+}
+
+export async function getValueByKey(array, key, keyName = 'key') {
+    return array.find(item => item[keyName] === key)?.value;
+}
+
+export function getFormattedDate(dateString) {
+    const date = new Date(dateString);
+    return date.toLocaleDateString('en-US', {
+        month: 'long',
+        day: 'numeric',
+        year: 'numeric'
+      });
+}
+
+export async function loadArticles() {
+    const graphqlEndpoint = await getConfigValueByKey('graphql-endpoint');
+    const timestamp = Date.now();
+    const response = await fetch(`${graphqlEndpoint}/articles-all?timestamp=${timestamp}`);
+    if (!response.ok) {
+        throw new Error('Failed to fetch articles');
+    }
+    const result = await response.json();
+    return result.data.articleList.items
+}
+
+export async function transformImageSrc(imageSrc) {
+    const siteName = await getConfigValueByKey('site-name');
+    imageSrc = imageSrc.replace(`/content/dam/${siteName}`, '');
     return imageSrc.replace('width=750&format=jpeg&optimize=medium', 'format=webp&optimize=high');
 }
