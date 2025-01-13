e1c166b468307ba327790220a84c1113ae02c7bc
François Fornaciari <francois.fornaciari@gmail.com>
2025-01-13 17:59:58 +0100

    update

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -5,6 +5,7 @@ export default async function decorate(block) {
     const config = readBlockConfig(block);
 
     const image1 = await transformImageSrc(config.image1);
+    console.log(image1);
     const image2 = await transformImageSrc(config.image2);
     const image3 = await transformImageSrc(config.image3);
 
@@ -12,7 +13,12 @@ export default async function decorate(block) {
         <div class="hero-banner">
             <div class="carousel">
                 <div class="slide active">
-                    <img src="${image1}" alt="${config.title1}" class="slide-image">
+                    <picture>
+                        <source type="image/webp" srcset="./media_1f9eb0b1f07d3bfdd15249cbdba0d62d00c70ecfa.jpeg?width=2000&format=webply&optimize=medium" media="(min-width: 600px)">
+                        <source type="image/webp" srcset="./media_1f9eb0b1f07d3bfdd15249cbdba0d62d00c70ecfa.jpeg?width=750&format=webply&optimize=medium">
+                        <source type="image/jpeg" srcset="./media_1f9eb0b1f07d3bfdd15249cbdba0d62d00c70ecfa.jpeg?width=2000&format=jpeg&optimize=medium" media="(min-width: 600px)">
+                        <img loading="lazy" alt="" src="./media_1f9eb0b1f07d3bfdd15249cbdba0d62d00c70ecfa.jpeg?width=750&format=jpeg&optimize=medium" width="2560" height="1440">
+                    </picture>
                     <div class="slide-content">
                         <h2 class="slide-title">${config.title1}</h2>
                         <p class="slide-price">${config.subtitle1}</p>
